c7400ea13f92c0436d501ccd04ff00ba3587cde0
Pavlinus <pavlin.kov@mail.ru>
2016-07-05 20:42:25 +0300

    Testing adaptive skills

---
+++ index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="ru-ru" dir="ltr">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link href="favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+<meta name="description" content="..." />
+<meta name="keywords" content="..." />
+<link rel="stylesheet" href="css/style.css" type="text/css" />
+<title>Volex</title>
+<!--[if IE]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<!-- jQuery library (served from Google) -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script src="scripts/jquery.bxslider.js"></script>
+<script src="scripts/effects.js"></script>
+<link href="css/jquery.bxslider.css" rel="stylesheet" />
+</head>
+<body id="page">
+  <section id="header">
+    <div class="layout">
+
+        <div id="lang">
+          <img src="images/eng.jpg"/>
+          <span class="lang-text">English</span>
+        </div>
+
+        <div id="header-menu">
+          <menu>
+            <li><a href="#">История типографии</a></li>
+            <li><a href="#">Оценка качества</a></li>
+            <li><a href="#">Новости</a></li>
+            <li><a href="#">Вакансии</a></li>
+            <li><a href="#">FAQ</a></li>
+          </menu>
+        </div>
+
+        <div id="search">
+          <form>
+            <input type="text" placeholder="Поиск..."/>
+            <input type="submit" value=""/>
+          </form>
+        </div>
+
+        <div id="social">
+          <img src="images/skype.png"/>
+          <img src="images/fb.png"/>
+          <img src="images/vk.png"/>
+        </div>
+      </div>
+      <div class="clr"></div>
+  </section>
+
+  <section id="middle">
+    <div class="layout">
+      <div class="layout">
+        <div class="logo"></div>
+
+        <div id="slider">
+          <ul class="bxslider">
+            <li><img src="images/slide001.jpg" /></li>
+            <li><img src="images/slide002.jpg" /></li>
+          </ul>
+        </div>
+
+        <div id="contacts">
+          <span class="phone">+7 (812) 603-25-25</span>
+          <span class="phone">+7 (812) 603-25-25</span>
+          <span class="sub-text">(звоните нам бесплатно)</span>
+          <span class="email">comm@lubavich.spb.ru</span>
+          <p>с 9:00 до 18:00</p>
+        </div>
+
+        <div id="order">
+          <a class="button" href="#">Заказать звонок</a>
+          <a class="button active" href="#">Сделать запрос в типографию</a>
+        </div>
+      </div>
+    </div>
+    <div class="clr"></div>
+  </section>
+
+  <section id="menu-nav">
+    <div class="layout">
+      <div id="nav-menu">
+        <menu>
+          <li><a href="#">Продукция</a></li>
+          <li><a href="#">Требования к макетам</a></li>
+          <li><a href="#">Портфолио</a></li>
+          <li><a href="#">Блог директора</a></li>
+          <li><a href="#">Статьи</a></li>
+          <li><a href="#">Контакты</a></li>
+        </menu>
+        <div class="clr"></div>
+      </div>
+    </div>
+  </section>
+
+  <section id="content">
+    <div class="layout">
+
+      <div id="products">
+        <h2>Продукция типографии</h2>
+
+        <div class="box5">
+          <img src="images/poster.png"/>
+          <p>Плакат</p>
+        </div>
+        <div class="box5">
+          <img src="images/poster.png"/>
+          <p>Плакат</p>
+        </div>
+        <div class="box5">
+          <img src="images/poster.png"/>
+          <p>Плакат</p>
+        </div>
+        <div class="box5">
+          <img src="images/poster.png"/>
+          <p>Плакат</p>
+        </div>
+        <div class="box5">
+          <img src="images/poster.png"/>
+          <p>Плакат</p>
+        </div>
+        <div class="box5">
+          <img src="images/poster.png"/>
+          <p>Плакат</p>
+        </div>
+        <div class="box5">
+          <img src="images/poster.png"/>
+          <p>Плакат</p>
+        </div>
+        <div class="box5">
+          <img src="images/poster.png"/>
+          <p>Плакат</p>
+        </div>
+      </div>
+
+      <div id="news">
+        <h2>Новости</h2>
+        <div class="news-item">
+          <img src="images/news001.jpg"/>
+          <div class="news-item-data">
+            <span class="date">21.10.2015</span>
+            <h4>Счастливый час</h4>
+            <p>Звоните с 10:00 до 11:00 каждый день до 1 декабря
+            2015 года</p>
+            <a class="details-link" href="#">Подробнее...</a>
+          </div>
+        </div>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </section>
+
+  <section id="menu-bottom">
+    <div class="layout">
+      <div id="nav-menu">
+        <menu>
+          <li><a href="#">Цены и оплата</a></li>
+          <li><a href="#">Доставка</a></li>
+          <li><a href="#">Оборудование</a></li>
+          <li><a href="#">Отзывы</a></li>
+          <li><a href="#">О нас</a></li>
+        </menu>
+        <div class="clr"></div>
+    </div>
+  </section>
+
+  <footer>
+    <div class="layout">
+      <div class="logo"></div>
+
+      <div id="footer-contacts">
+        <h4>Контакты</h4>
+        <span class="address">г. Санкт-Петербург, ул. Менделеевская, д. 9</span>
+        <div id="footer-phone-list">
+          <div class="list-item">
+            <img src="images/phone.png"/>
+            <span class="phone">+7 (812) 603-25-25</span>
+          </div>
+          <div class="clr"></div>
+          <div class="list-item">
+            <img src="images/phone.png"/>
+            <span class="phone">+7 (812) 603-25-25</span>
+          </div>
+          <div class="clr"></div>
+          <span class="sub-text">(звоните бесплатно)</span>
+          <span class="sub-text">с 9:00 до 18:00</span>
+        </div>
+
+        <div id="footer-email-list">
+          <div class="list-item">
+            <img src="images/mail.png"/>
+            <span class="email">comm@lubavich.spb.ru</span>
+            <span class="sub-text">(коммерческий отдел)</span>
+          </div>
+          <div class="clr"></div>
+          <div class="list-item">
+            <img src="images/mail.png"/>
+            <span class="email">comm@lubavich.spb.ru</span>
+            <span class="sub-text">(дизайн студия)</span>
+          </div>
+        </div>
+
+        <div class="clr"></div>
+
+        <div id="footer-skype">
+          <div class="list-item">
+            <img src="images/skype-footer.png"/>
+            <span class="skype-contact">skype:Ваш логин</span>
+          </div>
+        </div>
+      </div>
+
+      <div id="footer-social">
+        <div>
+          <h4>Мы в соц. сетях:</h4>
+          <div class="social-item">
+            <img src="images/vk_footer.png"/>
+            <span class="social-link">Группа в Вк</span>
+          </div>
+          <div class="social-item">
+            <img src="images/fb_footer.png"/>
+            <span class="social-link">Группа в Facebook</span>
+          </div>
+        </div>
+
+        <div id="footer-share">
+          <h4>Поделиться:</h4>
+          <div class="share">
+            <img src="images/skype.png"/>
+            <img src="images/skype.png"/>
+            <img src="images/skype.png"/>
+            <img src="images/skype.png"/>
+          </div>
+        </div>
+      </div>
+
+      <div id="subscribe">
+        <h4>Подписаться на рассылку:</h4>
+        <form>
+          <input type="text" placeholder="Введите Ваш e-mail"/>
+          <input type="submit" value="Подписаться"/>
+        </form>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
